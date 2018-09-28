f54e782d315dbea1c9991f344fc41be285a03bab
Dorota Czaplejewicz <dorota.czaplejewicz@puri.sm>
2018-09-28 15:38:01 +0000

    Crash GTK:
    WAYLAND_DEBUG=1 GDK_BACKEND=wayland GTK_IM_MODULE=wayland GTK_IM_MODULE_FILE=./gtk-install/lib/gtk-3.0/3.0.0/immodules.cache yad --entry
    
    and then
    
    WAYLAND_DEBUG=1 ../build-wlroots-Desktop-Debug/examples/input-method 2
    
    with rootston from this branch underneath

--- examples/input-method.c
+++ examples/input-method.c
@@ -1,24 +1,38 @@
 #define _POSIX_C_SOURCE 200809L
-#include <GLES2/gl2.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
 #include <unistd.h>
 #include <wayland-client.h>
 #include <wayland-egl.h>
 #include <wlr/render/egl.h>
 #include "input-method-unstable-v2-client-protocol.h"
+#include "text-input-unstable-v3-client-protocol.h"
 #include "xdg-shell-client-protocol.h"
-
-#include <linux/input-event-codes.h>
 
 /**
  * Usage: input-method [seconds]
  * Creates an input method using the input-method protocol.
  * Sends a couple of commands to the compositor whenever a new text input
- * appears. Displays received messages.
+ * appears. Displays state after receiving updates.
  * Time is optional and defines the delay between commits.
  */
+
+struct input_method_state {
+	enum zwp_text_input_v3_change_cause change_cause;
+	struct {
+		enum zwp_text_input_v3_content_hint hint;
+		enum zwp_text_input_v3_content_purpose purpose;
+	} content_type;
+	struct {
+		char *text;
+		uint32_t cursor;
+		uint32_t anchor;
+	} surrounding;
+};
 
 static int sleeptime = 0;
 
@@ -28,37 +42,98 @@ static struct wl_seat *seat = NULL;
 static struct zwp_input_method_manager_v2 *input_method_manager = NULL;
 static struct zwp_input_method_v2 *input_method = NULL;
 
+struct input_method_state pending;
+struct input_method_state current;
+
 static uint32_t serial = 0;
 bool active = false;
 bool pending_active = false;
 bool unavailable = false;
+bool running = false;
+
+uint32_t update_stage = 0;
+
+int timer_fd = 0;
+
+static void print_state_diff(struct input_method_state previous,
+		struct input_method_state future) {
+	if (previous.content_type.hint != future.content_type.hint) {
+		char *strs[] = { "COMPLETION", "SPELLCHECK", "AUTO_CAPITALIZATION",
+			"LOWERCASE", "UPPERCASE", "TITLECASE", "HIDDEN_TEXT",
+			"SENSITIVE_DATA", "LATIN", "MULTILINE"};
+		printf("content_type.hint:");
+		uint32_t hint = future.content_type.hint;
+		if (!hint) {
+			printf(" NONE");
+		}
+		for (unsigned i = 0; i < sizeof(strs) / sizeof(*strs); i++) {
+			if (hint & 1 << i) {
+				printf(" %s", strs[i]);
+			}
+		}
+		printf("\n");
+	}
+	if (previous.content_type.purpose != future.content_type.purpose) {
+		char *strs[] = { "NORMAL", "ALPHA", "DIGITS", "NUMBER", "PHONE", "URL",
+			"EMAIL", "NAME", "PASSWORD", "PIN", "DATE", "TIME", "DATETIME",
+			"TERMINAL" };
+		printf("content_type.purpose: %s\n", strs[future.content_type.purpose]);
+	}
+	if (!!previous.surrounding.text != !!future.surrounding.text
+			|| (previous.surrounding.text && future.surrounding.text
+				&& strcmp(previous.surrounding.text, future.surrounding.text) != 0)
+			|| previous.surrounding.anchor != future.surrounding.anchor
+			|| previous.surrounding.cursor != future.surrounding.cursor) {
+		char *text = future.surrounding.text;
+		if (!text) {
+			printf("Removed surrounding text\n");
+		} else {
+			printf("Surrounding text: %s\n", text);
+			uint32_t anchor = future.surrounding.anchor;
+			uint32_t cursor = future.surrounding.cursor;
+			if (cursor == anchor) {
+				char *temp = strndup(text, cursor);
+				printf("Cursor after %d: %s\n", cursor, temp);
+				free(temp);
+			} else {
+				if (cursor > anchor) {
+					uint32_t tmp = anchor;
+					anchor = cursor;
+					cursor = tmp;
+				}
+				char *temp = strndup(&text[cursor], anchor - cursor);
+				printf("Selection: %s\n", temp);
+				free(temp);
+			}
+		}
+	}
+	if (previous.change_cause != future.change_cause) {
+		char *strs[] = { "INPUT_METHOD", "OTHER" };
+		printf("Change cause: %s\n", strs[future.change_cause]);
+	}
+}
 
 static void handle_content_type(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2,
 		uint32_t hint, uint32_t purpose) {
-	// TODO: convert into names
-	printf("hint: %x, purpose: %x\n", hint, purpose);
+	pending.content_type.hint = hint;
+	pending.content_type.purpose = purpose;
 }
 
 static void handle_surrounding_text(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2,
 		const char *text, uint32_t cursor, uint32_t anchor) {
-	printf("Surrounding text: %s\n", text);
-	if (cursor < anchor) {
-		printf("Selection: %s\n", strndup(&text[cursor], anchor - cursor)); // TODO: leak
-	} else {
-		printf("Cursor after %d: %s\n", cursor, strndup(text, cursor)); // TODO: leak
-	}
+	free(pending.surrounding.text);
+	pending.surrounding.text = strdup(text);
+	pending.surrounding.cursor = cursor;
+	pending.surrounding.anchor = anchor;
 }
 
 static void handle_text_change_cause(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2,
 		uint32_t cause) {
-	if (cause) {
-		printf("Text modified externally\n");
-	}
+	pending.change_cause = cause;
 }
-
 
 static void handle_activate(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2) {
@@ -72,68 +147,128 @@ static void handle_deactivate(void *data,
 
 static void handle_unavailable(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2) {
-	printf("IM disappeared");
-	// FIXME: destroy
+	printf("IM disappeared\n");
 	zwp_input_method_v2_destroy(zwp_input_method_v2);
 	input_method = NULL;
+	running = false;
 }
 
 static void im_activate(void *data,
 		struct zwp_input_method_v2 *id) {
-	printf("Activated IM\n");
-	wl_display_roundtrip(display);
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
-	zwp_input_method_v2_preedit_string(input_method, "Preedit", 2, 4);
-	zwp_input_method_v2_commit(input_method, serial);
-	wl_display_roundtrip(display);
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
-	zwp_input_method_v2_preedit_string(input_method, "Præedit2", strlen("Pr"), strlen("Præed"));
-	zwp_input_method_v2_commit_string(input_method, "_Commit_");
-	zwp_input_method_v2_commit(input_method, serial);
-	wl_display_roundtrip(display);
-	return;
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
-	zwp_input_method_v2_commit_string(input_method, "_CommitNoPreed_");
-	zwp_input_method_v2_commit(input_method, serial);
-	wl_display_roundtrip(display);
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
+	update_stage = 0;
+}
 
-	zwp_input_method_v2_commit_string(input_method, "_WaitNo_");
-	zwp_input_method_v2_delete_surrounding_text(input_method, strlen("_CommitNoPreed_"), 0);
-	zwp_input_method_v2_commit(input_method, serial);
-	wl_display_roundtrip(display);
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
-	zwp_input_method_v2_preedit_string(input_method, "PreedWithDel", strlen("Preed"), strlen("Preed"));
-	zwp_input_method_v2_delete_surrounding_text(input_method, strlen("_WaitNo_"), 0);
-	zwp_input_method_v2_commit(input_method, serial);
-	wl_display_roundtrip(display);
-	sleep(sleeptime);
-	wl_display_roundtrip(display);
-	zwp_input_method_v2_delete_surrounding_text(input_method, strlen("mit_"), 0);
-	zwp_input_method_v2_commit(input_method, serial);
+static void timer_arm(unsigned seconds) {
+	printf("Timer armed\n");
+	struct itimerspec spec = {
+		.it_interval = {0},
+		.it_value = {
+			.tv_sec = seconds,
+			.tv_nsec = 0
+		}
+	};
+	if (timerfd_settime(timer_fd, 0, &spec, NULL)) {
+		fprintf(stderr, "Failed to arm timer: %s\n", strerror(errno));
+	}
+}
+
+static void do_updates() {
+	printf("Update %d\n", update_stage);
+	switch (update_stage) {
+	case 0:
+		// FIXME: save surrounding text
+		zwp_input_method_v2_preedit_string(input_method, "Preedit", 2, 4);
+		zwp_input_method_v2_commit(input_method, serial);
+		// don't expect an answer, preedit doesn't change anything visible
+		timer_arm(sleeptime);
+		update_stage++;
+		return;
+	case 1:
+		zwp_input_method_v2_preedit_string(input_method, "Præedit2", strlen("Pr"), strlen("Præed"));
+		zwp_input_method_v2_commit_string(input_method, "_Commit_");
+		zwp_input_method_v2_commit(input_method, serial);
+		update_stage++;
+		break;
+	case 2:
+		if (strcmp(current.surrounding.text, "_Commit_") != 0) {
+			return;
+		}
+		zwp_input_method_v2_commit_string(input_method, "_CommitNoPreed_");
+		zwp_input_method_v2_commit(input_method, serial);
+		timer_arm(sleeptime);
+		update_stage++;
+		break;
+	case 3:
+		if (strcmp(current.surrounding.text, "_Commit__CommitNoPreed_") != 0) {
+			return;
+		}
+		zwp_input_method_v2_commit_string(input_method, "_WaitNo_");
+		zwp_input_method_v2_delete_surrounding_text(input_method, strlen("_CommitNoPreed_"), 0);
+		zwp_input_method_v2_commit(input_method, serial);
+		update_stage++;
+		break;
+	case 4:
+		if (strcmp(current.surrounding.text, "_Commit__WaitNo_") != 0) {
+			return;
+		}
+		zwp_input_method_v2_preedit_string(input_method, "PreedWithDel", strlen("Preed"), strlen("Preed"));
+		zwp_input_method_v2_delete_surrounding_text(input_method, strlen("_WaitNo_"), 0);
+		zwp_input_method_v2_commit(input_method, serial);
+		update_stage++;
+		break;
+	case 5:
+		if (strcmp(current.surrounding.text, "_Commit_") != 0) {
+			return;
+		}
+		zwp_input_method_v2_delete_surrounding_text(input_method, strlen("mit_"), 0);
+		zwp_input_method_v2_commit(input_method, serial);
+		update_stage++;
+		break;
+	case 6:
+		if (strcmp(current.surrounding.text, "_Com") != 0) {
+			printf("Failed\n");
+		}
+		update_stage++;
+		break;
+	default:
+		printf("Submitted everything\n");
+		return;
+	};
+}
+
+static void handle_timer() {
+	printf("Timer dispatched at %d\n", update_stage);
+	do_updates();
 }
 
 static void im_deactivate(void *data,
 		struct zwp_input_method_v2 *context) {
-	printf("Deactivated IM\n");
+	// No special action needed
 }
 
 static void handle_done(void *data,
 		struct zwp_input_method_v2 *zwp_input_method_v2) {
 	bool prev_active = active;
 	serial++;
-	printf("FIXME: apply changes\n");
+	printf("Handle serial %d\n", serial);
+	if (active != pending_active) {
+		printf("Now %s\n", pending_active ? "active" : "inactive");
+	}
+	if (pending_active) {
+		print_state_diff(current, pending);
+	}
 	active = pending_active;
+	free(current.surrounding.text);
+	struct input_method_state default_state = {0};
+	current = pending;
+	pending = default_state;
 	if (active && !prev_active) {
 		im_activate(data, zwp_input_method_v2);
 	} else if (!active && prev_active) {
 		im_deactivate(data, zwp_input_method_v2);
 	}
+
+	do_updates();
 }
 
 static const struct zwp_input_method_v2_listener im_listener = {
@@ -199,14 +334,60 @@ int main(int argc, char **argv) {
 
 	input_method = zwp_input_method_manager_v2_get_input_method(
 		input_method_manager, seat);
-
+	running = true;
 	zwp_input_method_v2_add_listener(input_method, &im_listener, NULL);
 
-	wl_display_roundtrip(display);
-
-	while (wl_display_dispatch(display) != -1) {
-		// This space intentionally left blank
+	int display_fd = wl_display_get_fd(display);
+	timer_fd = timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC | TFD_NONBLOCK);
+	if (timer_fd < 0) {
+		fprintf(stderr, "Failed to start timer\n");
+		return EXIT_FAILURE;
+	}
+	int epoll = epoll_create1(EPOLL_CLOEXEC);
+	if (epoll < 0) {
+		fprintf(stderr, "Failed to start epoll\n");
+		return EXIT_FAILURE;
 	}
 
+	struct epoll_event epoll_display = {
+		.events = EPOLLIN | EPOLLOUT,
+		.data = {.fd = display_fd},
+	};
+	if (epoll_ctl(epoll, EPOLL_CTL_ADD, display_fd, &epoll_display)) {
+		fprintf(stderr, "Failed to epoll display\n");
+		return EXIT_FAILURE;
+	}
+
+	wl_display_roundtrip(display); // timer may be armed here
+
+	struct epoll_event epoll_timer = {
+		.events = EPOLLIN,
+		.data = {.fd = timer_fd},
+	};
+	if (epoll_ctl(epoll, EPOLL_CTL_ADD, timer_fd, &epoll_timer)) {
+		fprintf(stderr, "Failed to epoll timer\n");
+		return EXIT_FAILURE;
+	}
+
+	timer_arm(2);
+
+	struct epoll_event caught;
+	while (epoll_wait(epoll, &caught, 1, -1)) {
+		if (!running) {
+			printf("Exiting\n");
+			return EXIT_SUCCESS;
+		}
+		if (caught.data.fd == display_fd) {
+			if (wl_display_dispatch(display) == -1) {
+				break;
+			}
+		} else if (caught.data.fd == timer_fd) {
+			uint64_t expirations;
+			read(timer_fd, &expirations, sizeof(expirations));
+			handle_timer();
+		} else {
+			printf("Unknown source\n");
+		}
+	}
 	return EXIT_SUCCESS;
 }
